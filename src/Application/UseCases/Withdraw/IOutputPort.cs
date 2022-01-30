01d86c7d1832b823d3e3188ad4b8450fce537d95
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-01-30 21:43:53 +0200

    add Withdraw UseCase

---
+++ src/Application/UseCases/Withdraw/IOutputPort.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Accounts;
+using Domain.Debits;
+
+namespace Application.UseCases.Withdraw;
+
+public interface IOutputPort
+{
+    void OutOfFunds();
+
+    void Invalid();
+
+    void NotFound();
+
+    void Ok(Debit debit, Account account);
+}
---
+++ src/Application/UseCases/Withdraw/WithdrawPresenter.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Accounts;
+using Domain.Debits;
+
+namespace Application.UseCases.Withdraw;
+
+public sealed class WithdrawPresenter : IOutputPort
+{
+    public Account? Account { get; private set; }
+
+    public Debit? Debit { get; private set; }
+
+    public bool InvalidOutput { get; private set; }
+
+    public bool NotFoundOutput { get; private set; }
+
+    public bool OutOfFundsOutput { get; private set; }
+
+    public void Invalid()
+    {
+        InvalidOutput = true;
+    }
+
+    public void NotFound()
+    {
+        NotFoundOutput = true;
+    }
+
+    public void Ok(Debit debit, Account account)
+    {
+        Account = account;
+        Debit = debit;
+    }
+
+    public void OutOfFunds()
+    {
+        OutOfFundsOutput = true;
+    }
+}
