680914b9f9f2330be4380a854909b2a6a9201c8e
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-01-31 00:08:02 +0200

    add identity-server device

---
+++ identity-server/Quickstart/Device/DeviceAuthorizationInputModel.cs
@@ -0,0 +1,8 @@
+﻿using IdentityServer.Quickstart.Consent;
+
+namespace IdentityServer.Quickstart.Device;
+
+public class DeviceAuthorizationInputModel : ConsentInputModel
+{
+    public string UserCode { get; set; }
+}
---
+++ identity-server/Quickstart/Device/DeviceAuthorizationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using IdentityServer.Quickstart.Consent;
+
+namespace IdentityServer.Quickstart.Device;
+
+public class DeviceAuthorizationViewModel : ConsentViewModel
+{
+    public string UserCode { get; set; }
+
+    public bool ConfirmUserCode { get; set; }
+}
