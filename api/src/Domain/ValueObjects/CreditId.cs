c3b5c6f02191229fa8a37803ed36b22f96eb5d32
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-02-21 21:11:56 +0200

    Add CreditId and DebitId

---
+++ api/src/Domain/ValueObjects/CreditId.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public readonly struct CreditId : IEquatable<CreditId>
+    {
+        public Guid Id { get; }
+
+        public CreditId(Guid id)
+        {
+            Id = id;
+        }
+
+        public bool Equals(CreditId other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CreditId o && Equals(o);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString();
+        }
+
+        public static bool operator ==(CreditId left, CreditId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CreditId left, CreditId right)
+        {
+            return !left.Equals(right);
+        }
+    }
---
+++ api/src/Domain/ValueObjects/DebitId.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public readonly struct DebitId : IEquatable<DebitId>
+    {
+        public Guid Id { get; }
+
+        public DebitId(Guid id)
+        {
+            Id = id;
+        }
+
+        public bool Equals(DebitId other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DebitId o && Equals(o);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString();
+        }
+
+        public static bool operator ==(DebitId left, DebitId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(DebitId left, DebitId right)
+        {
+            return !left.Equals(right);
+        }
+    }
+
+    
+
+
+}
