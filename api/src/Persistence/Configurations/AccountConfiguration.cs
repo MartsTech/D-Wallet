854a90d6a9a801ef0c26702a8353867a9623ee4b
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-02-21 21:44:31 +0200

    add configurations edit

--- api/src/Persistence/Configurations/AccountConfiguration.cs
+++ api/src/Persistence/Configurations/AccountConfiguration.cs
@@ -4,9 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Persistence.Configurations
+namespace Persistence.Configurations;
+using Domain.Accounts;
+using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public sealed class AccountConfiguration : IEntityTypeConfiguration<Account>
 {
-    internal class AccountConfiguration
+    public void Configure(EntityTypeBuilder<Account> builder)
     {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        builder.ToTable("Account");
+
+        builder.Property(b => b.AccountId)
+           .HasConversion(
+               v => v.Id,
+               v => new AccountId(v))
+           .IsRequired();
+
+        builder.Property(b => b.ExternalUserId)
+           .UsePropertyAccessMode(PropertyAccessMode.FieldDuringConstruction);
+
+        builder.Property(credit => credit.Currency)
+          .HasConversion(
+              value => value.Code,
+              value => new Currency(value))
+          .IsRequired();
+
+        builder.HasMany(x => x.CreditsCollection)
+            .WithOne(b => b.Account!)
+            .HasForeignKey(b => b.AccountId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(x => x.DebitsCollection)
+            .WithOne(b => b.Account!)
+            .HasForeignKey(b => b.AccountId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
