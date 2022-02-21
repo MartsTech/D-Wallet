dfb0c1803cc4fc82f53af655dc90d03d825f088a
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-02-21 21:00:10 +0200

    add account entity

---
+++ api/src/Domain/Accounts/IAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Accounts;
+
+using Domain.Credits;
+using Domain.Debits;
+using Domain.ValueObjects;
+
+public interface IAccount
+{
+    AccountId AccountId { get; }
+
+    void Deposit(Credit credit);
+
+    void Withdraw(Debit debit);
+
+    Money GetCurrentBalance();
+
+    bool IsClosingAllowed();
+}
