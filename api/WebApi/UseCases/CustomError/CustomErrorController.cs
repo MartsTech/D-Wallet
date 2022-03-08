15dccdb1457691d73e01ef49c071244bfa084fa1
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-03-08 16:56:19 +0200

    add error case

---
+++ api/WebApi/UseCases/CustomError/CustomErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.UseCases.CustomError;
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public sealed class CustomErrorController : Controller
+{
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult CustomError()
+    {
+        CustomErrorResponse model = new()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        };
+
+        return View("~/UseCases/CustomError/CustomError.cshtml", model);
+    }
+}
