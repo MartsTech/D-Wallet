805337cc875e66d37e578eb5e6767423c792ed69
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-03-16 18:19:11 +0200

    add withdraw test

--- packages/api/tests/UnitTests/Transactions/Withdraw/WithdrawTests.cs
+++ packages/api/tests/UnitTests/Transactions/Withdraw/WithdrawTests.cs
@@ -9,6 +9,7 @@ using Persistence;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using UnitTests.Transactions.Withdraw;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -46,5 +47,34 @@ public sealed class WithdrawTests : IClassFixture<StandardFixture>
         Assert.True(result.IsSuccess);
         Assert.NotNull(result.Value);
     }
+    [Theory]
+    [ClassData(typeof(InvalidDataSetup))]
+    public async Task Error_When_Withdrawing_More_Money_Than_Money_In_Balance(decimal amount)
+    {
+        WithdrawInput input = new()
+        {
+            AccountId = SeedData.DefaultAccountId.Id,
+            Amount = amount,
+            Currency = "USD",
+           
 
+        };
+
+        WithdrawUseCase.Command command = new(input);
+
+        WithdrawUseCase.Handler handler = new(
+            _fixture.EntityFactory,
+            _fixture.AccountRepository,
+            _fixture.CurrencyExchange,
+            _fixture.UnitOfWork,
+            _fixture.UserService);
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        Assert.False(result.IsSuccess);
+        Assert.NotNull(result.Error);
+    }
 }
+
+
+    
