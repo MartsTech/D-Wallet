39d1ef40ad4bfc5a4c7ce704e46bd7f83633728d
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-03-16 19:50:34 +0200

    add transfer tests

---
+++ packages/api/tests/UnitTests/Transactions/Transferr/InvalidDataSetup.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTests.Transactions.Transferr;
+
+using Xunit;
+
+internal sealed class InvalidDataSetup : TheoryData<decimal>
+{
+    public InvalidDataSetup()
+    {
+        Add(-100);
+        Add(-10);
+        Add(-300);
+    }
+}
---
+++ packages/api/tests/UnitTests/Transactions/Transferr/TransferTests.cs
@@ -0,0 +1,78 @@
+﻿namespace UnitTests.Accounts.CloseAccount;
+
+using Application.UseCases.Accounts.CloseAccount;
+using Application.UseCases.Transactions.Transfer;
+using Application.UseCases.Transactions.Withdraw;
+using Domain.Accounts;
+using Domain.Credits;
+using Domain.ValueObjects;
+using FluentValidation.Results;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnitTests.Transactions.Transferr;
+using Xunit;
+using Xunit.Abstractions;
+
+public sealed class TransferrTests : IClassFixture<StandardFixture>
+{
+    private readonly StandardFixture _fixture;
+
+    public TransferrTests(StandardFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Theory]
+    [ClassData(typeof(ValidDataSetup))]
+    public async Task Transfer_Is_Successful(decimal amount)
+    {
+        TransferInput input = new()
+        {
+            Amount = amount,
+            OriginAccountId = SeedData.DefaultAccountId.Id,
+            DestinationAccountId = SeedData.DefaultAccountId.Id,
+            Currency = "USD"
+        };
+
+        TransferUseCase.Command command = new(input);
+
+        TransferUseCase.Handler handler = new(
+            _fixture.EntityFactory,
+            _fixture.AccountRepository,
+            _fixture.CurrencyExchange,
+            _fixture.UnitOfWork);
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
+        
+    }
+    [Theory]
+    [ClassData(typeof(InvalidDataSetup))]
+    public async Task Transfer_Is_Not_Successful_When_Transferring_Negative_Amount(decimal amount)
+    {
+        TransferInput input = new()
+        {
+            Amount = amount,
+            OriginAccountId = SeedData.DefaultAccountId.Id,
+            DestinationAccountId = SeedData.DefaultAccountId.Id,
+            Currency = "USD"
+        };
+
+        TransferUseCase.Command command = new(input);
+
+        TransferUseCase.Handler handler = new(
+            _fixture.EntityFactory,
+            _fixture.AccountRepository,
+            _fixture.CurrencyExchange,
+            _fixture.UnitOfWork);
+
+        TransferUseCase.CommandValidator validator = new();
+        ValidationResult validationResult = validator.Validate(command);
+
+        Assert.False(validationResult.IsValid);
+
+    }
+}
