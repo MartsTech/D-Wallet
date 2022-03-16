1952e85ca7d3993570ca67ce23ab8b2fdef3f5e9
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-03-16 16:13:19 +0200

    add withdraw tests

---
+++ packages/api/tests/UnitTests/Transactions/Withdraw/WithdrawTests.cs
@@ -0,0 +1,50 @@
+﻿namespace UnitTests.Accounts.CloseAccount;
+
+using Application.UseCases.Accounts.CloseAccount;
+using Application.UseCases.Transactions.Withdraw;
+using Domain.Accounts;
+using Domain.Credits;
+using Domain.ValueObjects;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+public sealed class WithdrawTests : IClassFixture<StandardFixture>
+{
+    private readonly StandardFixture _fixture;
+
+    public WithdrawTests(StandardFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Theory]
+    [ClassData(typeof(ValidDataSetup))]
+    public async Task Withdraw_Is_Successful(decimal amount)
+    {
+        WithdrawInput input = new()
+        {
+            AccountId = SeedData.DefaultAccountId.Id,
+            Amount = amount,
+            Currency = "USD"
+        };
+
+        WithdrawUseCase.Command command = new(input);
+
+        WithdrawUseCase.Handler handler = new(
+            _fixture.EntityFactory,
+            _fixture.AccountRepository,
+            _fixture.CurrencyExchange,
+            _fixture.UnitOfWork,
+            _fixture.UserService);
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Value);
+    }
+
+}
