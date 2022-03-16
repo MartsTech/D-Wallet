fac80e1ca16d11053fa36967b6ebf49cb9f0a930
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-03-16 18:47:48 +0200

    add deposit tests

---
+++ packages/api/tests/UnitTests/Transactions/Deposit/InvalidDataSetup.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTests.Transactions.Deposit;
+
+using Xunit;
+
+internal sealed class InvalidDataSetup : TheoryData<decimal>
+{
+    public InvalidDataSetup()
+    {
+        Add(-900m);
+        Add(-530m);
+        Add(-746m);
+    }
+}
