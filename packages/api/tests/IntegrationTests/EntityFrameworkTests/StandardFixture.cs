a48a7e4440f9c08ec153b71168633b4decbba087
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-03-17 19:06:44 +0200

    init integration tests

---
+++ packages/api/tests/IntegrationTests/EntityFrameworkTests/StandardFixture.cs
@@ -0,0 +1,30 @@
+﻿namespace IntegrationTests.EntityFrameworkTests;
+
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+
+public sealed class StandardFixture : IDisposable
+{
+    public StandardFixture()
+    {
+        const string connectionString =
+            "Server=localhost;User Id=root;Password=root;Database=d_wallet;";
+
+        MySqlServerVersion serverVersion = new(new Version(8, 0, 23));
+
+        DbContextOptions<DataContext> options = new DbContextOptionsBuilder<DataContext>()
+                .UseMySql(connectionString, serverVersion)
+                .Options;
+
+        Context = new DataContext(options);
+        Context.Database.EnsureCreated();
+    }
+
+    public DataContext Context { get; }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+    }
+}
